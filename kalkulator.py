3c7c5dc068c4158b83d3d00834eb3f6b77566ce4
Reza <98310028+rzazyx@users.noreply.github.com>
2024-06-14 13:06:40 +0700

    menambahkan pernyataan kondisi

--- kalkulator.py
+++ kalkulator.py
@@ -3,10 +3,8 @@
 print("PERHITUNGAN BMI (BODY MASS INDEX)")
 print("---------------------------------")
 
-berat_badan = input("Masukkan berat badan anda (kilogram): ")
-berat_badan = float(berat_badan)
-tinggi_badan = input("Masukkan tinggi badan anda (meter): ")
-tinggi_badan = float(tinggi_badan)
+berat_badan = float(input("Masukkan berat badan anda (kilogram): "))
+tinggi_badan = float(input("Masukkan tinggi badan anda (meter): "))
 
 bmi = berat_badan/(tinggi_badan**2)
 
@@ -14,8 +12,19 @@ berat_badan_ideal = dict()
 berat_badan_ideal ['minimal'] = 18.5*(tinggi_badan**2)
 berat_badan_ideal ['maksimal'] = 25*(tinggi_badan**2)
 
+if bmi< 18.5:
+      kategori = "Anda kekurangan berat badan"
+elif bmi< 25:
+      kategori = "Nilai BMI Anda adalah Normal"
+elif bmi< 30:
+      kategori = "Anda kelebihan Berat Badan"
+else:
+      kategori = "Anda mengalami Obesitas"
+
+print("\nHasil kalkulator BMI Anda adalah: ")
+print("----------------------------------")
 print(f"Nilai BMI anda adalah {bmi:.2f} kg/m^2")
-print("Nilai BMI normal adalah 18 kg/m^2 - 25 kg/m^2")
+print(kategori)
 print(f"Berat badan ideal anda dalam rentang " 
       f"{berat_badan_ideal['minimal']:.2f} kg- {berat_badan_ideal['maksimal']:.2f} kg")
 
